b36d668ab9271ab710766774a6d5104958098b7b
Johnny <949394771@qq.com>
2020-06-16 19:01:20 +0800

    signin page done

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,17 +1,26 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
+
 import { Layout, Avatar, Button, Space } from 'antd'
 import { UserOutlined } from '@ant-design/icons'
 const { Header } = Layout
 
 
 function MyHeader() {
+  const history = useHistory()
+
+  function SignOut() {
+    localStorage.clear()
+    history.push('/sign-in')
+  }
+
   return (
     <Header className="layout-header" >
       <span className="title">首页</span>
       <Space>
         <Avatar icon={<UserOutlined />} />
-        <span>13782890191</span>
-        <Button type='link' size='small'>登出</Button>
+        <span>{localStorage.getItem('account')}</span>
+        <Button type='link' size='small' onClick={SignOut} >登出</Button>
       </Space>
     </Header>
   )
--- src/views/SignIn.tsx
+++ src/views/SignIn.tsx
@@ -1,10 +1,41 @@
 import React from 'react'
+import { Form, Input, Button } from 'antd'
+import { useHistory } from 'react-router-dom'
+
+import '../assets/style/signin.sass'
+
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+}
+
 
 function SignIn() {
+  const history = useHistory()
+
+  function onFinish(data: any) {
+    console.log('Success:', data)
+    localStorage.setItem('account', data.account)
+    history.push('/')
+  }
+
+
   return (
-    <>
-      <div>signin</div>
-    </>
+    <div className="signin-wrap">
+      <Form className='signin-form' {...layout} name="basic" initialValues={{ remember: true }} onFinish={onFinish}>
+        <Form.Item label="account" name="account" rules={[{ required: true, message: '请输入账号！' }]}>
+          <Input />
+        </Form.Item>
+
+        <Form.Item label="password" name="password" rules={[{ required: true, message: '请输入密码！' }]}>
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8 }} style={{ marginBottom: 0 }}>
+          <Button type="primary" htmlType="submit">Submit</Button>
+        </Form.Item>
+      </Form>
+    </div>
   )
 }
 
